82c0a5778d54493985e26a59a0f16bc7725d7074
joshua stein <jcs@jcs.org>
2024-03-08 16:25:34 -0600

    App: Unescape "/" in HTML escaper, it makes things cleaner
    Surely this won't cause unforeseen XSS vulns

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,7 @@
 module Sinatra
   module HTMLEscapeHelper
     def h(text)
-      Rack::Utils.escape_html(text)
+      Rack::Utils.escape_html(text).to_s.gsub("&#x2F;", "/")
     end
   end
 
--- lib/sinatra_more/markup_plugin/format_helpers.rb
+++ lib/sinatra_more/markup_plugin/format_helpers.rb
@@ -27,7 +27,7 @@ module SinatraMore
 
     # Returns escaped text to protect against malicious content
     def escape_html(text)
-      Rack::Utils.escape_html(text)
+      Rack::Utils.escape_html(text).to_s.gsub("&#x2F;", "/")
     end
     alias h escape_html
     alias sanitize_html escape_html
