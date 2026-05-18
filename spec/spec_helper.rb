480e82ab411fc4e17a720aff9ff5d81580018c12
joshua stein <jcs@jcs.org>
2026-05-18 11:30:24 -0500

    spec: fix for newer AR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,15 +5,15 @@ ENV["APP_ENV"] = "test"
 
 require File.realpath(File.dirname(__FILE__) + "/../lib/app.rb")
 
-if File.exist?(_f = ActiveRecord::Base.connection_config[:database])
+if File.exist?(_f = ActiveRecord::Base.connection_db_config.database)
   File.unlink(_f)
 end
 
 ActiveRecord::Migration.verbose = false
-ActiveRecord::Base.connection.migration_context.migrate
+ActiveRecord::Base.connection_pool.migration_context.migrate
 
 include Rack::Test::Methods
 
 def app
-  Rubywarden::App
+  App
 end
